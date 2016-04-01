d03f26130e92d80091608f3e855026302e8ce842
Administrator <ylnreddi@gmail.com>
2016-04-01 21:49:06 +0100

    Green tests to compute the total for a basket with a mixture of Apples and Oranges

--- src/test/java/org/onlinestore/shoppingcart/service/CheckoutTest.java
+++ src/test/java/org/onlinestore/shoppingcart/service/CheckoutTest.java
@@ -13,34 +13,48 @@ import org.onlinestore.shoppingcart.model.Fruit;
 
 public class CheckoutTest {
 
-    private CheckoutService checkoutService;
+	private CheckoutService checkoutService;
 
 	@Before
 	public void setUp() throws Exception {
 		checkoutService = new CheckoutService();
 	}
 
-    @Test
-    public void total_should_calculate_correct_total_for_a_shopping_basket_for_a_single_apple() {
-        // Given
-        List<Fruit> shoppingBasket = Arrays.asList(Fruit.APPLE, Fruit.APPLE);
+	@Test
+	public void total_should_calculate_correct_total_for_a_shopping_basket_for_a_single_apple() {
+		// Given
+		List<Fruit> shoppingBasket = Arrays.asList(Fruit.APPLE, Fruit.APPLE);
 
-        // When
-        double total = checkoutService.total(shoppingBasket);
+		// When
+		double total = checkoutService.total(shoppingBasket);
 
-        // Then
-        assertThat("The total price is incorrect ", total, is(1.20));
-    }
-    
-    @Test
-    public void total_should_calculate_correct_total_for_a_shopping_basket_for_a_single_orange() {
-        // Given
-        List<Fruit> shoppingBasket = Arrays.asList(Fruit.ORANGE);
+		// Then
+		assertThat("The total price is incorrect ", total, is(1.20));
+	}
 
-        // When
-        double total = checkoutService.total(shoppingBasket);
+	@Test
+	public void total_should_calculate_correct_total_for_a_shopping_basket_for_a_single_orange() {
+		// Given
+		List<Fruit> shoppingBasket = Arrays.asList(Fruit.ORANGE);
 
-        // Then
-        assertThat("The total price is incorrect ", total, is(0.25));
-    }
+		// When
+		double total = checkoutService.total(shoppingBasket);
+
+		// Then
+		assertThat("The total price is incorrect ", total, is(0.25));
+	}
+
+	@Test
+	public void total_should_calculate_correct_total_for_a_shopping_basket_with_mixed_fruits() {
+		// Given
+		List<Fruit> shoppingBasket = Arrays.asList(Fruit.APPLE, Fruit.APPLE,
+				Fruit.ORANGE, Fruit.ORANGE, Fruit.APPLE);
+
+		// When
+		double total = checkoutService.total(shoppingBasket);
+
+		// Then
+		assertThat("The total price is incorrect ", total, is(2.30));
+	}
+
 }
